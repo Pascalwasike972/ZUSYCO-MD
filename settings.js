7340a8187b911ed3beea9f5ec9a38958c90d82d7
Pascalwasike972 <pascalwasike972@gmail.com>
2024-07-26 16:33:23 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,10 +16,10 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NUMBER: "254714480162",
+  SUDO_NUMBERS : "254714480162,94719574492",
+  OWNER_NAME: "『 PASCAL 』",
+  BOT_NAME : "PASCAL",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
@@ -48,7 +48,7 @@ module.exports = {
         NOT_WORKING : false,
   },
   READ : {
-     ALL_MESSAGES : true,
+     ALL_MESSAGES : false,
      ONLY_COMMANDS : false,
      STATUS : false,
   },
